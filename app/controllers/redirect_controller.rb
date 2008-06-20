96b95e3c61be28e367085eb9f4eb51fa5f1536a7
Ryan Johnson <arjini@gmail.com>
2008-06-20 12:36:11 -0700

    fixed catch all controller

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -1,19 +1,15 @@
 class RedirectController < ApplicationController
   def redirect
-    #if params[:url] && params[:url].match(/http\:\/\//)
-    #  render :text => 'The page has moved to: <a href="' + params[:url] + '">' + params[:url] + '</a><script>window.setTimeout(function(){window.location = "' + params[:url] + '"},2000);</script>'
-    #else
-      redirect_to params[:url], :status => 301
-    #end
+    redirect_to params[:url], :status => 301
   end
   
   def catch
     flash[:catch] = true
-    redirect_to :controller => 'page', :action => 'index', :status => 404
+    redirect_to :controller => 'page', :action => 'index'
   end
   
   def spam
     flash[:spam] = true
-    redirect_to :controller => 'page', :action => 'index', :status => 404
+    redirect_to :controller => 'page', :action => 'index'
   end
 end
