de1a1069a0bf1d2b8775ad348a7f8edebf0ab1a7
Ryan Johnson <arjini@gmail.com>
2008-06-20 12:34:39 -0700

    fixed redirection routes

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -1,6 +1,10 @@
 class RedirectController < ApplicationController
   def redirect
-    redirect_to params[:url], :status => 301
+    #if params[:url] && params[:url].match(/http\:\/\//)
+    #  render :text => 'The page has moved to: <a href="' + params[:url] + '">' + params[:url] + '</a><script>window.setTimeout(function(){window.location = "' + params[:url] + '"},2000);</script>'
+    #else
+      redirect_to params[:url], :status => 301
+    #end
   end
   
   def catch
